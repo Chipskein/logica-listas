5a1b2107b2f2fa40ba8a53bc8b32ecb34f5e0e00
Chipskein <abfn0905@gmail.com>
2023-08-12 22:42:41 -0300

    porra de 18

--- lista2/Ex18.java
+++ lista2/Ex18.java
@@ -6,10 +6,22 @@ public class Ex18 {
         do valor das vendas por ele efetuadas. Escrever um algoritmo que leia o número de
         carros por ele vendidos, o valor total de suas vendas, o salário fixo e o valor que ele
         recebe por carro vendido. Calcule e escreva o salário final do vendedor.
+
+        Exemplo
+        n_carros=10
+        valor_total_vendas=12000.00
+        salario_fixo=3000.00
+        comissao=50.00
+        (50*10)+3000+ 12000 * 0.05
     */
     public static void main(String[] agrs){
         Scanner in=new Scanner(System.in);
-
+        int n_carros=in.nextInt();
+        float valor_total_vendas=in.nextFloat();
+        float salario_fixo=in.nextFloat();
+        float comissao_carro=in.nextFloat();
+        float salario=salario_fixo+(comissao_carro*n_carros)+(valor_total_vendas*0.05f);
+        System.out.printf("Salario Total:%.2f\n",salario);
         in.close();
     }
 }
