1c42853af664c8f7daf3a3d4019500ef43a95619
Chipskein <abfn0905@gmail.com>
2023-08-15 22:17:41 -0300

    feito 24

---
+++ lista4/Ex24.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Ex24 {
+    /*
+         Escreva um algoritmo em Java que receba vários números e verifique se eles são ou
+        não quadrados perfeitos. O algoritmo termina a execução quando for digitado um
+        número menor ou igual a 0. (Um número é quadrado perfeito quando tem um
+        número inteiro como raiz quadrada.). Obs: não deve ser utilizado métodos da classe
+        Math.
+    */
+    public static void main(String[] args){
+        Scanner in=new Scanner(System.in);
+        int numero=1;
+        while(numero>0){
+            numero=in.nextInt();
+            boolean eh_quadrado=false;
+            for(int i=1;i<=numero;i++){
+                int n=i*i;
+                if(n==numero){
+                    eh_quadrado=true;
+                    break;
+                }
+            }
+            System.out.printf("Numero:%d\nQuadrado:%b\n",numero,eh_quadrado);
+        }
+        in.close();
+    }
+}
