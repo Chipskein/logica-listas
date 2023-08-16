2a36e31d196149ae1fcaec243dcf11cdfbadc22d
Chipskein <abfn0905@gmail.com>
2023-08-15 23:22:12 -0300

    feito 24

--- lista4/Ex24.java
+++ lista4/Ex24.java
@@ -1,27 +1,44 @@
 import java.util.Scanner;
 public class Ex24 {
     /*
-         Escreva um algoritmo em Java que receba vários números e verifique se eles são ou
-        não quadrados perfeitos. O algoritmo termina a execução quando for digitado um
-        número menor ou igual a 0. (Um número é quadrado perfeito quando tem um
-        número inteiro como raiz quadrada.). Obs: não deve ser utilizado métodos da classe
-        Math.
+        Criar um algoritmo em Java que leia um conjunto de informações (nome, sexo,
+        idade, peso e altura) dos atletas que participaram de uma olimpíada, e informar:
+        - O atleta do sexo masculino mais alto;
+        - A atleta do sexo feminino mais pesada;
+        - A média de idade dos atletas.
+        Deverão ser lidos dados dos atletas até que seja digitado o nome @ para um atleta 
     */
     public static void main(String[] args){
         Scanner in=new Scanner(System.in);
-        int numero=1;
-        while(numero>0){
-            numero=in.nextInt();
-            boolean eh_quadrado=false;
-            for(int i=1;i<=numero;i++){
-                int n=i*i;
-                if(n==numero){
-                    eh_quadrado=true;
-                    break;
-                }
+        String nome="";
+        int n_atletas=0;
+        float soma_idade=0.0f;
+        String nome_m_atleta_mais_alto="";
+        String nome_f_atleta_mais_pesada="";
+        float maior_peso=0.0f;
+        float maior_altura=0.0f;
+        while(!nome.equals("@")){
+            nome=in.next();
+            String sexo=in.next();
+            if(!sexo.equalsIgnoreCase("m")&&!sexo.equalsIgnoreCase("f")){
+                continue;
             }
-            System.out.printf("Numero:%d\nQuadrado:%b\n",numero,eh_quadrado);
+            int idade=in.nextInt();
+            float peso=in.nextFloat();
+            float altura=in.nextFloat();
+            if(sexo.equalsIgnoreCase("m")&&altura>=maior_altura){
+                maior_altura=altura;
+                nome_m_atleta_mais_alto=nome;
+            }
+            if(sexo.equalsIgnoreCase("f")&&peso>=maior_peso){
+                maior_peso=peso;
+                nome_f_atleta_mais_pesada=nome;
+            }
+            soma_idade+=idade;
+            n_atletas++;
         }
+        float media_idade = n_atletas>0 ? soma_idade/n_atletas:0;
+        System.out.printf("Atleta(M) mais alto(%.2f):%s\nAtleta(F) mais pesada(%.2f):%s\nMedia da idade dos atletas:%.2f\n",maior_altura,nome_m_atleta_mais_alto,maior_peso,nome_f_atleta_mais_pesada,media_idade);
         in.close();
     }
 }
--- lista4/Ex25.java
+++ lista4/Ex25.java
@@ -1,85 +1,27 @@
 import java.util.Scanner;
 public class Ex25 {
     /*
-        Existem três candidatos a uma vaga no Senado. Feita a eleição, os votos são
-        registrados em disco. O voto de cada eleitor foi codificado da seguinte forma:
-        Deseja-se saber:
-        - O número do candidato vencedor;
-        - O número de votos em branco;
-        - O número de votos nulos;
-        - O número de eleitores que compareceram às urnas.
-        Admite-se que não são possíveis empates. Então, construa um algoritmo em Java
-        que execute esta tarefa, sabendo que o voto de cada eleitor é lido pelo Scanner. O
-        voto finalizador tem código –1, e não deve ser computado.
+         Escreva um algoritmo em Java que receba vários números e verifique se eles são ou
+        não quadrados perfeitos. O algoritmo termina a execução quando for digitado um
+        número menor ou igual a 0. (Um número é quadrado perfeito quando tem um
+        número inteiro como raiz quadrada.). Obs: não deve ser utilizado métodos da classe
+        Math.
     */
     public static void main(String[] args){
         Scanner in=new Scanner(System.in);
-        int voto=1;
-        int cod_finalizador=-1;
-        int eleitores=0;
-        int votos_cad1=0;
-        int votos_cad2=0;
-        int votos_cad3=0;
-        int votos_branco=0;
-        int votos_nulos=0;
-        int cod_vencedor=0;
-        boolean empate=false;
-        while(voto!=cod_finalizador||empate){
-            System.out.printf("Vote\n(1)Cleiton\n(2)Kleber\n(3)Jonh\n(4)Em Branco\n");
-            voto=in.nextInt();
-            switch(voto){
-                case 1:
-                    votos_cad1++;
+        int numero=1;
+        while(numero>0){
+            numero=in.nextInt();
+            boolean eh_quadrado=false;
+            for(int i=1;i<=numero;i++){
+                int n=i*i;
+                if(n==numero){
+                    eh_quadrado=true;
                     break;
-                case 2:
-                    votos_cad2++;
-                    break;
-                case 3:
-                    votos_cad3++;
-                    break;
-                case 4:
-                    votos_branco++;
-                    break;
-                case -1:
-                    if(votos_cad1>=votos_cad2&&votos_cad1>=votos_cad3){
-                        if(votos_cad1==votos_cad2||votos_cad1==votos_cad3){
-                            System.out.println("Empate Isso nao pode terminar assim");
-                            empate=true;
-                            continue;
-                        }
-                        cod_vencedor=1;
-                        continue;
-                    }
-                    if(votos_cad2>=votos_cad1&&votos_cad2>=votos_cad3){
-                        if(votos_cad2==votos_cad1||votos_cad2==votos_cad3){
-                            System.out.println("Empate Isso nao pode terminar assim");
-                            empate=true;
-                            continue;
-                        }
-                        cod_vencedor=2;
-                        continue;
-                    }
-                    if(votos_cad3>=votos_cad2&&votos_cad3>=votos_cad1){
-                        if(votos_cad3==votos_cad1||votos_cad3==votos_cad2){
-                            System.out.println("Empate Isso nao pode terminar assim");
-                            empate=true;
-                            continue;
-                        }
-                        cod_vencedor=3;
-                        continue;
-                    }
-                    break;//stop the count
-                default:
-                    votos_nulos++;
-                    break;
+                }
             }
-            if(!empate){
-                eleitores++;
-            };
-            empate=false;
+            System.out.printf("Numero:%d\nQuadrado:%b\n",numero,eh_quadrado);
         }
-        System.out.printf("(1)Cleiton - %d\n(2)Kleber - %d\n(3)Jonh - %d\n(4)Em Branco - %d\n( )Nulo - %d\nEleitores - %d\n",votos_cad1,votos_cad2,votos_cad3,votos_branco,votos_nulos,eleitores);
-        System.out.printf("Codigo Vencedor:%d\nVotos Brancos:%d\nVotos Nulos:%d\nEleitores:%d\n",cod_vencedor,votos_branco,votos_nulos,eleitores);
         in.close();
     }
 }
