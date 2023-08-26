039af6e954b0da43ea2e88437ed6422b6a252818
Bruno Nascimento <abfn0905@gmail.com>
2023-08-25 21:33:01 -0300

    Add files via upload

--- lista6/Ex9.java
+++ lista6/Ex9.java
@@ -7,25 +7,24 @@ public class Ex9 {
     */
     public static void main(String[] args){
         Scanner in=new Scanner(System.in);
-        int numero=in.nextInt();
-        String numero_str=numero+"";
-        char milhar=numero_str.charAt(0);
-        char centena=numero_str.charAt(1);
-        char dezena=numero_str.charAt(2);
-        char unidade=numero_str.charAt(3);
-        String milhar_centena_str=(milhar+""+centena);
-        int milhar_centena=Integer.parseInt(milhar_centena_str);
-        String dezena_unidade_str=(dezena+""+unidade);
-        int dezena_unidade=Integer.parseInt(dezena_unidade_str);
-        int soma=milhar_centena+dezena_unidade;
-        int soma_quadrado=soma*soma;
-        boolean numero_tem_caracteristica= soma_quadrado==numero;
-        if(numero_tem_caracteristica){
-            System.out.printf("(%d+%d)=%d %d²=%d %d=%d\n",milhar_centena,dezena_unidade,soma,soma,soma_quadrado,soma_quadrado,numero);
-        } else{
-            System.out.printf("(%d+%d)=%d %d²=%d %d!=%d\n",milhar_centena,dezena_unidade,soma,soma,soma_quadrado,soma_quadrado,numero);
+        int limite=9999;
+        for(int numero=1000;numero<=limite;numero++){
+            String numero_str=numero+"";
+            char milhar=numero_str.charAt(0);
+            char centena=numero_str.charAt(1);
+            char dezena=numero_str.charAt(2);
+            char unidade=numero_str.charAt(3);
+            String milhar_centena_str=(milhar+""+centena);
+            int milhar_centena=Integer.parseInt(milhar_centena_str);
+            String dezena_unidade_str=(dezena+""+unidade);
+            int dezena_unidade=Integer.parseInt(dezena_unidade_str);
+            int soma=milhar_centena+dezena_unidade;
+            int soma_quadrado=soma*soma;
+            boolean numero_tem_caracteristica= soma_quadrado==numero;
+            if(numero_tem_caracteristica){
+                System.out.printf("(%d+%d)=%d %d²=%d %d=%d\n",milhar_centena,dezena_unidade,soma,soma,soma_quadrado,soma_quadrado,numero);
+            }
         }
-        
         in.close();
     }    
 }
