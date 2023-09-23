55d24f86ec257923e23109f44f3fb71f3ddfc817
Chipskein <abfn0905@gmail.com>
2023-09-22 21:01:28 -0300

    Terminar Ex2 dps

---
+++ lista7/Ex2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Ex2{
+    /*
+      Escreva um programa em Java que dado o mês do ano, 
+      indique quantos dias tem o
+      mês informado. Obs: Atente-se para o caso dos anos bissextos
+      //31=[janeiro,março,maio,julho,agosto,outubro,dezembro]
+      //28=[fevereiro]
+      //29=[fevereiro]
+      //30=[abril,junho,setembro,novembro]
+    */
+    public static void main(String args[]){
+        Scanner in=new Scanner(System.in);
+        
+        in.close();
+    }
+}
