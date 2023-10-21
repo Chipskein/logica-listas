0656ca8babfba68fc0cd6b3af42d623988b947d2
Chipskein <abfn0905@gmail.com>
2023-10-20 22:06:18 -0300

    fix a porra do 9

---
+++ simulado/Ex9.java
@@ -0,0 +1,37 @@
+import java.awt.Color;
+import java.awt.Graphics;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Ex9 extends JPanel {
+    
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.setColor(Color.BLACK);
+        //parte de cima
+        for(int v=0;v<=400;v+=20){
+            g.drawLine(v,v,400-v,0);
+        }
+        for(int v=0;v<=400;v+=20){
+            g.drawLine(v,400,400-v,400-v);
+        }
+    }
+
+
+    public static void main(String[] args) {
+        
+        JFrame janela = new JFrame("Figuras retas");
+        janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        janela.setSize(410, 440);
+
+        Ex9 painel = new Ex9();
+        painel.setBounds(0, 0, 400, 400);
+        
+        janela.add(painel);
+        janela.setVisible(true);
+        
+    }
+
+}
