cd985ff9c449979bea485042f51085716dd7e251
Chipskein <abfn0905@gmail.com>
2023-10-21 16:00:13 -0300

    feito ex10

--- simulado/Ex10.java
+++ simulado/Ex10.java
@@ -4,15 +4,20 @@ import java.awt.Graphics;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class Ex9 extends JPanel {
+public class Ex10 extends JPanel {
     
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         g.setColor(Color.BLACK);
-        for(int v=0;v<=400;v+=20){
-            g.drawLine(v,v,400-v,0);
-            g.drawLine(v,400,400-v,400-v);
+        for (int v = 0; v <= 400; v = v + 20) {
+            //parte esquerda
+            g.drawLine(200 - v/2, v, v, 400);
+            //parte direita
+            g.drawLine(200 + v/2, v, 400-v, 400);
+            //parte de cima
+            g.drawLine(200-v/2,v,400-v/2,400-v);
+            g.drawLine(200+v/2,v,v/2,400-v);
         }
     }
 
@@ -23,7 +28,7 @@ public class Ex9 extends JPanel {
         janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         janela.setSize(410, 440);
 
-        Ex9 painel = new Ex9();
+        Ex10 painel = new Ex10();
         painel.setBounds(0, 0, 400, 400);
         
         janela.add(painel);
